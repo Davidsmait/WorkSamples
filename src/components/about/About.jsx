a6fadbbe2b019040f20511eca5797470ebb032e2
Davidsmait <deivod_halo@hotmail.com>
2023-01-30 02:26:39 -0600

    icons instead of text in AboutSection

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,11 @@ import './about.css'
 import ME from '../../assets/me-about.jpg'
 import {AiOutlineCode} from 'react-icons/ai'
 import {TbCode} from 'react-icons/tb'
+import {SiJavascript, SiExpress, SiTailwindcss} from 'react-icons/si'
+import {AiFillHtml5  } from 'react-icons/ai'
+import {DiCss3, DiNpm, DiMongodb} from 'react-icons/di'
+import {FaReact, FaNodeJs , FaGit} from 'react-icons/fa'
+
 
 const About = () => {
   return (
@@ -21,17 +26,29 @@ const About = () => {
           <div className="about__cards">
             <article className='about__card' >
               <TbCode className="about__icon "/>
-              <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <h5>Frontend</h5>
+              <small>
+              <SiJavascript />
+              <DiCss3 />
+              <AiFillHtml5 />
+              <FaReact />
+              <SiTailwindcss />
+              </small>
             </article>
             <article className='about__card' >
               <AiOutlineCode className="about__icon "/>
-              <h5>Proyects</h5>
-              <small>20+ Completed</small>
+              <h5>Backend & Tools</h5>
+              <small>
+                <FaNodeJs />
+                <DiMongodb />
+                <SiExpress />
+                <DiNpm />
+                <FaGit />
+              </small>
             </article>
           </div>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quo voluptatum modi. Minus ut sit laboriosam, nam nulla labore vitae, quo voluptate quaerat at ducimus suscipit rerum, cumque fugiat debitis!</p>
+          <p>// Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quo voluptatum modi. Minus ut sit laboriosam, nam nulla labore vitae, quo voluptate quaerat at ducimus suscipit rerum, cumque fugiat debitis!</p>
 
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
