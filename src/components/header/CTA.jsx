a5396cbc8fa5ca7780d406fe5f7346620cf03045
Davidsmait <deivod_halo@hotmail.com>
2023-02-01 00:40:22 -0600

    GitHub instead of cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'> Download CV </a>
+      <a href="https://github.com/Davidsmait" target="__blank" download className='btn'> GitHub </a>
       <a href="https://api.whatsapp.com/send/?phone=527711006533&text&type=phone_number&app_absent=0" target="__blank" className='btn btn-primary'>Let's Talk </a>
     </div>
   )
